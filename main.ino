1fc18056dd1d4b0e160e7914bb639a3fb3084754
Rohan Bharatia <144117280+Rohan-Bharatia@users.noreply.github.com>
2023-12-11 08:43:06 -0500

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,28 @@
+#include <CapacitiveSensor>
+
+CapacitiveSensor CapSens = CapacitiveSensor(3, 2);
+const int threshold = 1000;
+const int ledPin = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  long sensVal = capSens.capacitiveSensor(30);
+
+  Serial.println(sensVal);
+
+  if(sensVal > threshold)
+  {
+    digitalWrite(ledPin, HIGH);
+  } else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  
+  delay(1000);
+}
